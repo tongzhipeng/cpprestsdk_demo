fe3ae9ca3fb5deb5b9b9bb9fa81c946c3023738f
tongzhipeng <tongzhipeng5688@gmail.com>
2020-07-02 14:08:34 +0800

    cpprest demo工程

---
+++ README.md
@@ -0,0 +1,13 @@
+# cpprestsdk 使用例子
+
+## 内容列表
+- [cpprestsdk 使用例子](#cpprestsdk-使用例子)
+  - [内容列表](#内容列表)
+  - [静态库使用](#静态库使用)
+
+## 静态库使用
+1. 获取cpprestsdk 静态库 :  vcpkg install cpprestsdk:x86-windows-static
+2. 编译本工程
+   1. mkdir build && cd build
+   2. cmake ../ -DVCPKG_ROOT_DIR=${your_vcpkg_root_dir}
+   3. cmake --build .
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+
+int main(int, char**) {
+    auto fileStream = std::make_shared<ostream>();
+    pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
+    {
+        *fileStream = outFile;
+
+        // Create http_client to send the request.
+        http_client client(U("http://www.bing.com/"));
+
+        // Build request URI and start the request.
+        uri_builder builder(U("/search"));
+        builder.append_query(U("q"), U("cpprestsdk github"));
+        return client.request(methods::GET, builder.to_string());
+    }).then([=](http_response response)
+    {
+        printf("Received response status code:%u\n", response.status_code());
+
+        // Write response body into the file.
+        return response.body().read_to_end(fileStream->streambuf());
+    }).then([=](size_t)
+    {
+        return fileStream->close();
+    });;
+}
